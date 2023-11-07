3612ce3dd49c5d206eb77c3a51deb602d65851e6
AlexProstolucky <alexprostolucky@gmail.com>
2023-11-07 20:30:51 +0200

    Добавьте файлы проекта.

---
+++ H2_H3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+internal class Program
+{
+    static async System.Threading.Tasks.Task Main(string[] args)
+    {
+        string url = "https://auto.ria.com/uk/newauto/marka-audi/model-rs7-sportback/year-2023/";  
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string htmlContent = await response.Content.ReadAsStringAsync();
+
+                    HtmlDocument htmlDocument = new HtmlDocument();
+                    htmlDocument.LoadHtml(htmlContent);
+
+                    var h2Nodes = htmlDocument.DocumentNode.SelectNodes("//h2");
+                    var h3Nodes = htmlDocument.DocumentNode.SelectNodes("//h3");
+
+                    if (h2Nodes != null)
+                    {
+                        foreach (var node in h2Nodes)
+                        {
+                            Console.WriteLine("Заголовок (h2): " + node.InnerText);
+                        }
+                    }
+
+                    if (h3Nodes != null)
+                    {
+                        foreach (var node in h3Nodes)
+                        {
+                            Console.WriteLine("Заголовок (h3): " + node.InnerText);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Помилка при виконанні запиту. Код статусу: " + (int)response.StatusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Виникла помилка: " + ex.Message);
+            }
+        }
+    }
+}
